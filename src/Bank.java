8937c8d977205ce3583cc278e4ce41ef85071535
Saad Karim Eddin <78364708+karim-eddin@users.noreply.github.com>
2023-05-06 14:30:12 +0100

    Add files via upload

---
+++ src/Bank.java
@@ -0,0 +1,17 @@
+
+public class Bank {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account Karim = new Account("Karim Eddin", "A00001");
+
+
+        Account Brad = new Account("Brad", "B00002");
+        Brad.showMenu();
+
+
+    }
+
+}
+
